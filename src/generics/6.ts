6b9ac359366b7cc1af9d1e886408dc49363b83cb
Alona93Hryhorieva <151534732+Alona93Hryhorieva@users.noreply.github.com>
2024-08-08 23:17:52 +0300

    add generic 5,6

--- src/generics/6.ts
+++ src/generics/6.ts
@@ -1,12 +1,10 @@
 // У вас є тип Form, який містить інформацію про форму, включаючи поле errors. Ви хочете створити новий тип Params, який включає всі поля з Form, крім errors.
-
 // type Errors = {
 //   email?: string[];
 //   firstName?: string[];
 //   lastName?: string[];
 //   phone?: string[];
 // };
-
 // type Form = {
 //   email: string | null;
 //   firstName: string | null;
@@ -14,7 +12,24 @@
 //   phone: string | null;
 //   errors: Errors;
 // };
-
-// // Реалізуйте Params так,
-// // щоб унеможливити поле 'errors' з типу Form
+// Реалізуйте Params так,
+// /щоб унеможливити поле 'errors' з типу Form
 // type Params = Form;
+type Errors = {
+  email?: string[];
+  firstName?: string[];
+  lastName?: string[];
+  phone?: string[];
+};
+type Form = {
+  email: string | null;
+  firstName: string | null;
+  lastName: string | null;
+  phone: string | null;
+  errors: Errors;
+};
+
+type Params = Pick<Form, "email" | "firstName" | "lastName" | "phone">;
+
+// або  Omit, виключаючи поле 'errors'
+type Params = Omit<Form, "errors">;
